9bd35addb24cceb2dd0a5c33ffe36c5fb7965fcb
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2015-01-08 08:48:39 +0100

    Moving to pytest

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
 
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 from readchar import __version__
 
 
 def read_description():
     with open('README.rst') as fd:
         return fd.read()
+
+
+class PyTest(TestCommand):
+    user_options = [
+        ('pytest-args=', 'a', "Arguments to pass to py.test"),
+    ]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args or ['--cov-report=term-missing'])
+        sys.exit(errno)
 
 
 setup(name='readchar',
@@ -35,6 +58,19 @@ setup(name='readchar',
       packages=find_packages(exclude=['tests', 'venv']),
       include_package_data=True,
       zip_safe=False,
+      cmdclass={'test': PyTest},
+      tests_require=[
+          'flake8 >= 2.2.3',
+          'doublex >= 1.8.1',
+          'pexpect >= 3.3',
+
+          'pytest >= 2.6.2',
+          'pytest-cov >= 1.8.0',
+
+          'python-coveralls >= 2.4.2',
+
+          'wheel >= 0.24.0',
+      ],
       install_requires=[
-        ],
-      )
+      ],
+)
