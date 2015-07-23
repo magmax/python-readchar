09dd84ce66817d241f1873cc650421769f97e104
Brandon <brandonkeithbiggs@gmail.com>
2015-07-23 06:30:15 +0200

    Added a dict of special keys in windows. All but 6 keys work and those 6 have 2 meanings. Find the dict of keys in the keys.py. The reason why there are 2 dicts is because there are 2 processing areas for keys and they overlap. The next task is to convert the linux and mac sets to fit into the same dict format.

--- readchar/readchar_windows.py
+++ readchar/readchar_windows.py
@@ -3,12 +3,23 @@
 # http://code.activestate.com/recipes/134892/#c9
 # Thanks to Stephen Chappell
 import msvcrt
-
+from keys import windows_keys, windows_special_keys
 
 def readchar(blocking=False):
-    "Get a single character on Windows."
+	"""Get a single character on Windows."""
+	ch = msvcrt.getch()
+	if ch in '\x00\xe0':
+		ch = msvcrt.getch()
+		ch = windows_special_keys.get(ch, ch)
+	ch = repr(ch)[1:-1]
+	if ch.startswith('\\') or ch == ' ':
+		ch = windows_keys.get(ch, ch)
+	return ch
 
-    ch = msvcrt.getch()
-    if ch in '\000\xe0':
-        ch = msvcrt.getch()
-    return repr(ch)[1:-1]
+if __name__ == '__main__':
+	while True:
+		c = readchar()
+		print(c)
+
+		if c == 'e':
+			break
