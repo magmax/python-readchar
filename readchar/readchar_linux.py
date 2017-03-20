588f1be6669fced519e3392170adc2893608f1e5
suresttexas00 <suresttexas00@live.com>
2017-03-20 18:25:19 -0400

    test

--- readchar/readchar_linux.py
+++ readchar/readchar_linux.py
@@ -17,9 +17,9 @@ def get_char(test_stream=None):
     charbuffer = ''
     while True:
         if charbuffer in key.ESCAPE_SEQUENCES:
-            char1 = test_stream or readchar(False)
+            char1 = str(test_stream) or readchar(False)
         else:
-            char1 = test_stream or readchar()
+            char1 = str(test_stream) or readchar()
 
         # return if escape sequence is finished (or not an escape sequence).
         if (charbuffer + char1) not in key.ESCAPE_SEQUENCES:
