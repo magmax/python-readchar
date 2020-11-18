82630b336b4f9b39d389adad3e1df61b2c993965
Mateusz Piotrowski <0mp@FreeBSD.org>
2020-11-18 18:52:09 +0100

    Use readchar_linux for FreeBSD as well
    It seems to work just fine, similarly to Darwin.

--- readchar/readchar.py
+++ readchar/readchar.py
@@ -9,6 +9,8 @@ if sys.platform.startswith('linux'):
     from .readchar_linux import readchar
 elif sys.platform == 'darwin':
     from .readchar_linux import readchar
+elif sys.platform.startswith('freebsd'):
+    from .readchar_linux import readchar
 elif sys.platform in ('win32', 'cygwin'):
     import msvcrt
     from .readchar_windows import readchar
