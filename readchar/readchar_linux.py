47a85f2d118ebbfadf1d9404cba3772d9301ef70
Daniel Vogt <daniel@vogt.ml>
2021-03-08 08:56:42 +0100

    Undo accidental change. Fix Enter Key

--- readchar/readchar_linux.py
+++ readchar/readchar_linux.py
@@ -11,7 +11,7 @@ def readchar():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
-        tty.setcbreak(sys.stdin.fileno())
+        tty.setraw(sys.stdin.fileno())
         ch = sys.stdin.read(1)
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
