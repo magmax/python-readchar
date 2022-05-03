ccd4838b720c141e59f7bea523135bb43f148a96
Jan Wille <mail@janwille.de>
2022-05-03 13:36:33 +0200

    only import platfrom from sys, not full package

--- readchar/__init__.py
+++ readchar/__init__.py
@@ -1,14 +1,14 @@
-import sys
+from sys import platform
 
 
-if sys.platform.startswith("linux"):
+if platform.startswith("linux"):
     from .read_linux import readchar, readkey
     from . import key_linux as key
-elif sys.platform in ("win32", "cygwin"):
+elif platform in ("win32", "cygwin"):
     from .read_windows import readchar, readkey
     from . import key_windows as key
 else:
-    raise NotImplementedError("The platform %s is not supported yet" % sys.platform)
+    raise NotImplementedError(f"The platform {platform} is not supported yet")
 
 
 __all__ = ["readchar", "readkey", "key"]
