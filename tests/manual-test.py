9fbd90676e527cea720ed7b253f2ccbb68858156
Jan Wille <mail@janwille.de>
2022-05-05 23:43:42 +0200

    added testscript

---
+++ tests/manual-test.py
@@ -0,0 +1,17 @@
+from readchar import key, readkey
+
+
+# construct an inverted code -> key-name mapping
+# we need to revese the items so that aliases won't overrive the original name later on
+known_keys = {v: k for k, v in reversed(vars(key).items()) if not k.startswith("__")}
+
+
+while True:
+    data = readkey()
+
+    if data in known_keys:
+        print(f"got {known_keys[data]}", end="")
+    else:
+        print(data, end="")
+
+    print(f' - \\x{"".join([f"{ord(c):02x}" for c in data])}')
