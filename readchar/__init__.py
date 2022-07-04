8d61ee1e46ca42fa169487c3afbfa6fc2fa5eae5
c0d3d3v <c0d3d3v@mag-keinen-spam.de>
2022-07-04 15:36:34 +0200

    fix __all__
    https://docs.python.org/3/tutorial/modules.html#importing-from-a-package

--- readchar/__init__.py
+++ readchar/__init__.py
@@ -1,9 +1,8 @@
 """Library to easily read single chars and key strokes"""
 
 __version__ = "0.0.0-dev"
+__all__ = ["readchar", "readkey", "key"]
 
 
 from . import key
 from .readchar import readchar, readkey
-
-__all__ = [readchar, readkey, key]
