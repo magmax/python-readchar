b79d366f7b459fda6d1e0c0996f2812645ce8f1b
trombonehero <jonathan.anderson@mun.ca>
2022-07-04 15:47:23 +0200

    os support: FreeBSD

--- readchar/key.py
+++ readchar/key.py
@@ -5,6 +5,7 @@ from . import platform
 if (
     platform.startswith("linux")
     or platform == "darwin"
+    or platform.startswith("freebsd")
 ):
     from .posix_key import *
 elif platform in ("win32", "cygwin"):
