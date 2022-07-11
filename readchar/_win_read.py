aa78f8ce33a943af47db5c34ca75ec189a49afce
Jan Wille <mail@janwille.de>
2022-07-11 16:29:31 +0200

    windows: fix `readchar()` function

--- readchar/_win_read.py
+++ readchar/_win_read.py
@@ -1,30 +1,14 @@
 import msvcrt
-import sys
 
 import key
 
 
-# Initially taken from:
-# http://code.activestate.com/recipes/134892/#c9
-# Thanks to Stephen Chappell
-win_encoding = "mbcs"
-XE0_OR_00 = "\x00\xe0"
+def readchar() -> str:
+    """Reads a single character from the input stream.
+    Blocks until a character is available."""
 
-
-def readchar():
-    "Get a single character on Windows."
-
-    while msvcrt.kbhit():
-        msvcrt.getch()
-    ch = msvcrt.getch()
-    # print('ch={}, type(ch)={}'.format(ch, type(ch)))
-    # while ch.decode(win_encoding) in unicode('\x00\xe0', win_encoding):
-    while ch.decode(win_encoding) in XE0_OR_00:
-        # print('found x00 or xe0')
-        msvcrt.getch()
-        ch = msvcrt.getch()
-
-    return ch if sys.version_info.major > 2 else ch.decode(encoding=win_encoding)
+    # manual byte decoding because some bytes in windows are not utf-8 encodable.
+    return chr(int.from_bytes(msvcrt.getch(), "big"))
 
 
 # Windows uses scan codes for extended characters. This dictionary translates
