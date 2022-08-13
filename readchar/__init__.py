ab4434f2181f1c68be8fcf9ab9989f23f120b905
Cube707 <Cube707@users.noreply.github.com>
2022-08-13 17:59:22 +0000

    increment version after release

--- readchar/__init__.py
+++ readchar/__init__.py
@@ -1,6 +1,6 @@
 """Library to easily read single chars and key strokes"""
 
-__version__ = "4.0.1-dev0"
+__version__ = "4.0.2-dev0"
 __all__ = ["readchar", "readkey", "key"]
 
 from sys import platform
