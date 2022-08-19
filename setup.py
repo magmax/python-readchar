c6a7ddd9a740dfd7c83381e1866bdf14b5c2a24e
Jan Wille <mail@janwille.de>
2022-08-19 16:47:55 +0200

    move name to setup.py
    this is needed so github can detect the package name and display the
    dependency tree correctly

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 
 setup(
+    # The following is only here to make GitHubs dependency detection happy:
+    name="readchar",
     # Author can not be read from setup.cfg as it causes encoding problems during
     # windows installs
-    author="Miguel Ángel García"
+    author="Miguel Ángel García",
 )
